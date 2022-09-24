b3184f7c692656d3ea510694e6014090006ab8bf
sumathi <sumathigonugunta16>
2022-09-24 12:09:57 +0530

    line count in txt file

---
+++ file_read_txt.py
@@ -0,0 +1,58 @@
+# read file
+f = open("sample.txt", "r",encoding="utf8",)
+# print(f.readlines())
+
+# get the number of lines
+l1 = f.readlines()
+print(l1)
+print(len(l1))
+
+# get linecount without empty lines
+## option 1 using while
+i = 0 # line index
+c = 0 # line count without emty lines
+while i<len(l1):
+    if l1[i] != "\n":
+        c += 1
+    i += 1
+print(f"no of lines {i}")
+print(f"no of lines without empty lines {c}")
+
+## option 2 using for
+
+c = 0 # line count without emty lines
+for line in l1:
+    if line != '\n':
+        c += 1
+print(f"no of lines {len(l1)}")
+print(f"no of lines without empty lines {c}")
+
+## option 3 using list comprehension
+
+l2 = [line for line in l1 if line != '\n']
+print(f"no of lines {len(l1)}")
+print(f"no of lines without empty lines {len(l2)}")
+
+# get the lines in a list
+l2 = [line for line in l1 if line != '\n']
+print(f"no of lines without empty lines {l2}")
+print(len(l2))
+
+# add l1 elements to l2
+l2=[]
+n=len(l1)
+for i in range(0,n):
+    l2.append(l1[i])
+    print(f"content in l2 is {l2}")
+
+
+l3 = []
+for j in l1:
+    if j != "\n":
+        l3.append(j)
+print(f"l3 content is {l3}")
+# get the number of words
+
+# get the number of words in a list
+
+# craete a dict with word as key and no of occurences in the file as values
